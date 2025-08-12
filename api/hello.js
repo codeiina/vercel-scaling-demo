2921f064ab7ac5a016d16d2cfebde5451a8090bd
codeiina <raihananisrina04@gmail.com>
2025-08-12 12:23:57 +0700

    Update and rename halo.js to hello.js

--- api/hello.js
+++ api/hello.js
@@ -1,7 +1,11 @@
 // api/hello.js
+const INSTANCE_ID = Math.random().toString(36).slice(2);
+
 export default function handler(req, res) {
+  console.log(INSTANCE: ${INSTANCE_ID} | TIME: ${new Date().toISOString()});
   res.status(200).json({
     message: "Hello from Vercel instance!",
+    instance: INSTANCE_ID,
     time: new Date().toISOString(),
   });
 }
